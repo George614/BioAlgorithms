74e552b5a3db01056085e30493c2d25ec2883892
Zhizhuo Yang <yangzhizhuo@gmail.com>
2019-03-30 14:48:38 -0400

    finished PSO & APSO

--- ParticleSwarm.py
+++ ParticleSwarm.py
@@ -3,8 +3,6 @@ Implementation of the Particle Swarm Optimization algorithm, both standard versi
 """
 
 import numpy as np
-import random
-import math
 
 def func1(x):
     total=0
@@ -15,11 +13,12 @@ def func1(x):
 class Particle:
     def __init__(self, bounds, dim):
         self.dim = dim
-        self.position = np.random.uniform(bounds[0], bounds[1], self.dim)  # particle position
+        self.bounds = bounds
+        self.position = np.random.uniform(self.bounds[0], self.bounds[1], self.dim)  # particle position
         self.velocity = np.zeros(self.dim)  # particle velocity
-        self.pos_best = None  # best position individual
-        self.err_best = -1  # best error individual
-        self.error = -1       # error individual
+        self.pos_best = None  # best individual position
+        self.err_best = -1  # best individual error
+        self.error = -1       # individual error at each iteration
 
     # evaluate current fitness
     def evaluate(self, obj_func):
@@ -30,9 +29,9 @@ class Particle:
             self.err_best = self.error
 
     # update new particle velocity
-    def update_velocity(self, inertia=0.5, # constant inertia weight (how much to weigh the previous velocity)
-                        beta = 2,  # cognitive constant
-                        alpha = 2,  # social constant
+    def update_velocity(self, inertia=0.5,  # constant inertia weight (how much to weigh the previous velocity)
+                        beta=2,   # cognitive constant
+                        alpha=2,  # social constant
                         pos_best_g=None):
         epsilon1 = np.random.rand()
         epsilon2 = np.random.rand()
@@ -40,58 +39,56 @@ class Particle:
         vel_social = alpha * epsilon1 * (pos_best_g - self.position)
         self.velocity = inertia * self.velocity + vel_cognitive + vel_social
 
+    # APSO update velocity
+    def acc_update_velocity(self, beta=0.5, alpha0=0.5, it=0, gamma=0.95, pos_best_g=None):
+        epsilon = np.random.normal()  # drawn from a Gaussian distribution
+        alpha = alpha0 * np.exp(-gamma*it)
+        vel_social = beta * (pos_best_g - self.position)
+        vel_cognitive = alpha * epsilon
+        self.velocity = self.velocity + vel_cognitive + vel_social
+
     # update the particle position based off new velocity updates
-    def update_position(self, bounds):
-        for i in range(0, num_dimensions):
-            self.position[i] = self.position[i] + self.velocity[i]
-
-            # adjust maximum position if necessary
-            if self.position[i] > bounds[i][1]:
-                self.position[i] = bounds[i][1]
-
-            # adjust minimum position if neseccary
-            if self.position[i] < bounds[i][0]:
-                self.position[i] = bounds[i][0]
-
+    def update_position(self):
+        self.position = self.position + self.velocity
+        # adjust position to be within the boundaries
+        self.position[self.position > self.bounds[1]] = self.bounds[1]
+        self.position[self.position < self.bounds[0]] = self.bounds[0]
 
 class PSO:
-    def __init__(self, obj_func, x0, bounds, num_particles, maxiter):
-        global num_dimensions
-        # x0 is initial position of
-        num_dimensions = len(x0)
+    def __init__(self, obj_func, dimension, bounds, pop_size, max_iter, alpha,
+                 acc=False, inertia=0.5, beta=2, gamma=0.95):
         err_best_g = -1  # best error for group
         pos_best_g = []  # best position for group
-
-        # establish the swarm
+        # build the swarm
         swarm = []
-        for i in range(0, num_particles):
-            swarm.append(Particle(x0))
+        for i in range(pop_size):
+            swarm.append(Particle(bounds, dimension))
 
         # begin optimization loop
-        i = 0
-        while i < maxiter:
-            # print i,err_best_g
-            # cycle through particles in swarm and evaluate fitness
-            for j in range(0, num_particles):
+        it = 0
+        while it < max_iter:
+            # evaluate fitness for each particle
+            for j in range(pop_size):
                 swarm[j].evaluate(obj_func)
-
-                # determine if current particle is the best (globally)
+                # determine if current particle is the global best
                 if swarm[j].error < err_best_g or err_best_g == -1:
                     pos_best_g = list(swarm[j].position)
                     err_best_g = float(swarm[j].error)
+            # update velocities and positions
+            if acc:  # Use APSO
+                for k  in range(pop_size):
+                    swarm[k].acc_update_velocity(beta, alpha, it, gamma, pos_best_g)
+                    swarm[k].update_position()
+                it += 1
+            else:   # Use PSO
+                for k in range(pop_size):
+                    swarm[k].update_velocity(inertia, beta, alpha, pos_best_g)
+                    swarm[k].update_position()
+                it += 1
 
-            # cycle through swarm and update velocities and position
-            for j in range(0, num_particles):
-                swarm[j].update_velocity(pos_best_g)
-                swarm[j].update_position(bounds)
-            i += 1
+        # print out results
+        print("best position: ", pos_best_g)
+        print("best error: ", err_best_g)
 
-        # print final results
-        print('FINAL:')
-        print(pos_best_g)
-        print(err_best_g)
-
-
-initial = [5, 5]  # initial starting location [x1,x2...]
-bounds = [(-10, 10), (-10, 10)]  # input bounds [(x1_min,x1_max),(x2_min,x2_max)...]
-PSO(func1, initial, bounds, num_particles=15, maxiter=30)
+bound = [-10,10]
+PSO(func1, 2, bound, pop_size=15, max_iter=50, alpha=0.5, beta=0.5, acc=True)
